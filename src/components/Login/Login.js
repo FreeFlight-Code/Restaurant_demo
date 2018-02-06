dc23a27dd7835744824e034bc6aab93b9f248895
FreeFlight-Code <davidfisc@hotmail.com>
2018-02-06 15:40:49 -0700

    login page css good

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-import logo from './communityBank.svg';
-import './Login.css';
-
-
-export default class Login extends Component {
+import logo from '../../images/logo.svg';
+import './Login.css'; export default
+    class Login extends Component {
     render() {
         return (
-            <div className='App'>  
-                <img src={logo} alt=""/>
-                <a href='http://localhost:3030/auth'><button>Login</button></a>
-            </div> 
+            <div className='App'>
+                <div id='loginContainer'>
+                    <div id='logoSection'>
+                        <img src={logo} alt=""></img>
+                        <h4>Welcome to</h4>
+                        <h2>Good Eats</h2>
+                    </div>
+                    <div id='right'>
+                    <div>
+                        <label for="username"></label>Username:
+                        <input type="search" name="username" id="username" />
+                    </div>
+                    <div>
+                        <label for="password">Password:</label>
+                        <input type="search" name="Password" id="" />
+                    </div>
+                        <div>
+                            <a href='http://localhost:3030/auth'>
+                                <button>Login</button>
+                            </a>
+                            <a href='http://localhost:3030/auth'>
+                                <button>Register</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
