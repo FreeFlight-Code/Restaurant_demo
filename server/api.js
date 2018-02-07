0b850f45fcda8c87485a039b46f907f66aa6dad8
FreeFlight-Code <davidfisc@hotmail.com>
2018-02-07 15:47:39 -0700

    updated api.js file

--- server/api.js
+++ server/api.js
@@ -1,20 +1,12 @@
 module.exports = {
-
+//read all products
     allProducts: function (req, res, next){
         let db = req.app.get('db')
         db.allProducts().then(allProducts=>{
             res.status(200).send(allProducts)
         })
     },
-
-    product: function (req, res, next){
-        let { productId } = req.params.id;
-        let db = req.app.get('db')
-        db.product(productId).then(product=>{
-            res.status(200).send(product)
-        })
-    },
-
+//create product
     addProduct: function (req, res, next){
         let { name, description, price } = req.body;
         let db = req.app.get('db')
@@ -22,17 +14,25 @@ module.exports = {
             res.status(200).send(product)
         })
     },
-
+//read product
+    product: function (req, res, next){
+        let { productId } = req.params.id;
+        let db = req.app.get('db')
+        db.product(productId).then(product=>{
+            res.status(200).send(product)
+        })
+    },
+//update product
     editProduct: function (req, res, next){
         let { id, name, description, price } = req.body;
         let db = req.app.get('db')
-        db.editProduct([id, name, description, price]).then(product=>{
+        db.product([id, name, description, price]).then(product=>{
             res.status(200).send(true)
         }).catch((err)=>{
             res.status(400).send(err)
         })
     },
-
+//delete product
     deleteProduct: function (req, res, next){
         let id = req.params.id;
         let db = req.app.get('db')
@@ -42,7 +42,5 @@ module.exports = {
             res.status(400).send(err)
         })
     },
-
-
 
 }
